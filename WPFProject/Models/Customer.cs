70e328103519478acd69adf5647e121b8fee3262
LakatosBotond <lakatosbotond@turr.hu>
2026-05-28 13:38:26 +0200

    Add project files.

---
+++ WPFProject/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFProject.Models
+{
+    public class Customer { 
+        public int Id { get; set; } 
+        public int UserId { get; set; }
+        public string PhoneNumber { get; set; } 
+        public string Address { get; set; } 
+        public string City { get; set; } 
+        public string Country { get; set; } 
+        public DateTime BirthDate { get; set; } 
+}
